d460023b0c8f2534cc0e749759c3a8dace06d873
Azimbek <omar.azimbek@gmail.com>
2023-12-26 01:39:13 +0600

    0.3.4 Commit

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c, h, a, r;
+        c = 'c';
+        h = 'h';
+        a = 'a';
+        r = 'r';
+
+        int intC = c;
+        int intH = h;
+        int intA = a;
+        int intR = r;
+
+        System.out.println("Integer Representation of " + c + " is " + intC);
+        System.out.println("Integer Representation of " + h + " is " + intH);
+        System.out.println("Integer Representation of " + a + " is " + intA);
+        System.out.println("Integer Representation of " + r + " is " + intR);
     }
 }
